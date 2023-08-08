16c1ceb51fbeed61eca5afd47db6570df3cf897d
Sergio Tafoya <sergio.tafoya@gmail.com>
2023-08-08 13:30:08 -0600

    SEGUNDA

---
+++ help/.output/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,69 @@
+const client_manifest = {
+  "../node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
+    "resourceType": "style",
+    "file": "error-404.a6f724cc.css",
+    "src": "../node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
+  },
+  "../node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-404.7033789b.js",
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "../node_modules/@nuxt/ui-templates/dist/templates/error-404.vue"
+  },
+  "error-404.a6f724cc.css": {
+    "file": "error-404.a6f724cc.css",
+    "resourceType": "style"
+  },
+  "../node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+    "resourceType": "style",
+    "file": "error-500.d92554dd.css",
+    "src": "../node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+  },
+  "../node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-500.d13e0fa9.js",
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "../node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+  },
+  "error-500.d92554dd.css": {
+    "file": "error-500.d92554dd.css",
+    "resourceType": "style"
+  },
+  "../node_modules/nuxt/dist/app/entry.css": {
+    "resourceType": "style",
+    "file": "entry.95fa068a.css",
+    "src": "../node_modules/nuxt/dist/app/entry.css"
+  },
+  "../node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "css": [
+      "entry.95fa068a.css"
+    ],
+    "dynamicImports": [
+      "../node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "../node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "file": "entry.9c17c9f7.js",
+    "isEntry": true,
+    "src": "../node_modules/nuxt/dist/app/entry.js",
+    "_globalCSS": true
+  },
+  "entry.95fa068a.css": {
+    "file": "entry.95fa068a.css",
+    "resourceType": "style"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
